908154bf11858656da4e3134d4794d29a2bd6f24
TANAEL BUDIMAN <tanael.budiman@binus.ac.id>
2022-01-10 17:28:38 +0700

    atasan perubahan pelaksana belum di test. memperbaiki kajian resiko di pelaksanaCAPA Pembatalan, tambah perbaikan, dan perubahan.

--- B7_CAPA_Online/Controllers/AtasanPelaksanaController.cs
+++ B7_CAPA_Online/Controllers/AtasanPelaksanaController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using B7_CAPA_Online.Models;
+using B7_CAPA_Online.Scripts.DataAccess;
+using Dapper;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static B7_CAPA_Online.Models.KoordinatorModel;
 
 namespace B7_CAPA_Online.Controllers
 {
     public class AtasanPelaksanaController : Controller
     {
+        readonly DataAccess DAL = new DataAccess();
         // GET: AtasanPelaksana
         public ActionResult Index()
         {
@@ -32,6 +38,18 @@ namespace B7_CAPA_Online.Controllers
         public ActionResult AtasanPelaksanaUpdateCAPA()
         {
             return View();
+        }
+
+        public ActionResult AtasanPelaksanaApprovalPerubahanTindakan()
+        {
+            return View();
+        }
+
+        public ActionResult DynamicController(DynamicModel Models, string spname)
+        {
+            var parameters = new DynamicParameters(Models.Model);
+            return Json(DAL.StoredProcedure(parameters, spname));
+
         }
     }
 }
