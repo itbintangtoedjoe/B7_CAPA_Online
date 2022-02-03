ab1b7f027a31ee443871f6f98dcb86576ad93b31
TANAEL BUDIMAN <tanael.budiman@binus.ac.id>
2022-02-03 10:43:11 +0700

    sudah menambahkan email sender ke atasan pelaksana, penilaian efektivitas, approval koordinator, pelaksanaan capa.

--- B7_CAPA_Online/Controllers/AtasanPelaksanaController.cs
+++ B7_CAPA_Online/Controllers/AtasanPelaksanaController.cs
@@ -1,6 +1,9 @@
 ﻿using B7_CAPA_Online.Models;
 using B7_CAPA_Online.Scripts.DataAccess;
+using B7_CAPA_Online.Scripts.SMTP;
 using Dapper;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -58,5 +61,61 @@ namespace B7_CAPA_Online.Controllers
             return Json(DAL.StoredProcedure(parameters, spname));
 
         }
-    }
+
+        public ActionResult SubmitApproval(DynamicModel Param)
+        {
+            DynamicParameters parameters = new DynamicParameters(Param.Model);
+            string Recipient = DAL.StoredProcedure(parameters, "[dbo].[SP_EMAIL]");
+
+            var list = new List<Recipients>();
+            var objects = JsonConvert.DeserializeObject(Recipient.ToString()); // parse as array  
+
+            foreach (var item in ((JArray)objects))
+            {
+                list.Add(new Recipients
+                {
+                    NO_CAPA = item.Value<string>("NoCAPA")
+                    ,
+                    Email = item.Value<string>("Email")
+                    ,
+                    KategoriCAPA = item.Value<string>("KategoriCAPA")
+                    ,
+                    ToEmpName = item.Value<string>("ToEmpName")
+                    ,
+                    TriggerCAPA = item.Value<string>("TriggerCAPA")
+                    ,
+                    Lokasi = item.Value<string>("Lokasi")
+                    ,
+                    StatusCAPA = item.Value<string>("StatusCAPA")
+                    ,
+                    PIC = item.Value<string>("PIC")
+                    ,
+                    DeskripsiMasalah = item.Value<string>("DeskripsiMasalah")
+                    ,
+                    CreateBy = item.Value<string>("Requestor")
+                });
+            }
+
+            if (list[0].NO_CAPA != null)
+            {
+                EmailSender emailSender = new EmailSender();
+                foreach (var value in list)
+                {
+                    emailSender.SendEmail(new Dictionary<string, object> {
+                        {"Nama_Aplikasi", "CAPA" }, // ini hardcode 
+                        {"Kategori", "PICReminder" }, // ini hardcode
+                        {"KategoriCAPA", value.KategoriCAPA }, // dari list diatas
+                        {"ToEmpName", value.ToEmpName }, // dari list diatas
+                        {"Recipient", Recipient}, // recipient object list dari atas
+                        {"TriggerCAPA", value.TriggerCAPA}, // ini dari list diatas
+                        {"Lokasi", value.Lokasi}, // dari list diatas
+                        {"StatusCAPA", value.StatusCAPA}, // ini dari list diatas
+                        {"PIC", value.PIC}, // ini dari list diatas
+                        {"CreateBy", value.CreateBy}, // ini dari list diatas
+                        {"DeskripsiMasalah", value.DeskripsiMasalah } // ini dari list diatas
+                    });
+                }
+            }
+            return Json(Recipient);}
+        }
 }
