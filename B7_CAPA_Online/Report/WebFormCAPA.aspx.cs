c069715d09eb24adc0b39a2cf3a04d73e61cc83e
ray <benedictusraynaldi@gmail.com>
2022-02-03 16:32:44 +0700

    fixing report ada data source yang duplikat, mengambil tanggal untuk approval tindakan perbaikan dan pencegahan

--- B7_CAPA_Online/Report/WebFormCAPA.aspx.cs
+++ B7_CAPA_Online/Report/WebFormCAPA.aspx.cs
@@ -173,40 +173,40 @@ namespace B7_CAPA_Online.Report
                         throw ex;
                 }
 
-                ReportDataSource DataSource = new ReportDataSource("B7_CAPA_ONLINEDataSetHeader1", DT);
+                ReportDataSource DataSource = new ReportDataSource("~/DataSource/B7_CAPA_ONLINEDataSetHeader1.Designer.cs", DT);
                 this.ReportViewer1.LocalReport.DataSources.Clear();
                 this.ReportViewer1.LocalReport.DataSources.Add(DataSource);
                 ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSetHeader1", DT));
 
-                ReportDataSource DataSource1 = new ReportDataSource("B7_CAPA_ONLINEDataSetAnalisa", DT1);
+                ReportDataSource DataSource1 = new ReportDataSource("~/DataSource/B7_CAPA_ONLINEDataSetAnalisa.Designer.cs", DT1);
                 this.ReportViewer1.LocalReport.DataSources.Add(DataSource1);
                 ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSetAnalisa", DT1));
 
-                ReportDataSource DataSource2 = new ReportDataSource("B7_CAPA_ONLINEDataSetRoot", DT2);
+                ReportDataSource DataSource2 = new ReportDataSource("~/DataSource/B7_CAPA_ONLINEDataSetRoot.Designer.cs", DT2);
                 this.ReportViewer1.LocalReport.DataSources.Add(DataSource2);
                 ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSetRoot", DT2));
 
-                ReportDataSource DataSource3 = new ReportDataSource("B7_CAPA_ONLINEDataSetPerbaikan", DT3);
+                ReportDataSource DataSource3 = new ReportDataSource("~/DataSource/B7_CAPA_ONLINEDataSetPerbaikan.Designer.cs", DT3);
                 this.ReportViewer1.LocalReport.DataSources.Add(DataSource3);
                 ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSetPerbaikan", DT3));
 
-                ReportDataSource DataSource4 = new ReportDataSource("B7_CAPA_ONLINEDataSetPencegahan", DT4);
+                ReportDataSource DataSource4 = new ReportDataSource("~/DataSource/B7_CAPA_ONLINEDataSetPencegahan.Designer.cs", DT4);
                 this.ReportViewer1.LocalReport.DataSources.Add(DataSource4);
                 ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSetPencegahan", DT4));
 
-                ReportDataSource DataSource5 = new ReportDataSource("B7_CAPA_ONLINEDataSetPelaksana", DT5);
+                ReportDataSource DataSource5 = new ReportDataSource("~/DataSource/B7_CAPA_ONLINEDataSetPelaksana.Designer.cs", DT5);
                 this.ReportViewer1.LocalReport.DataSources.Add(DataSource5);
                 ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSetPelaksana", DT5));
 
-                ReportDataSource DataSource6 = new ReportDataSource("B7_CAPA_ONLINEDataSetBukti", DT6);
+                ReportDataSource DataSource6 = new ReportDataSource("~/DataSource/B7_CAPA_ONLINEDataSetBukti", DT6);
                 this.ReportViewer1.LocalReport.DataSources.Add(DataSource6);
                 ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSetBukti", DT6));
 
-                ReportDataSource DataSource7 = new ReportDataSource("B7_CAPA_ONLINEDataSetKajian", DT7);
+                ReportDataSource DataSource7 = new ReportDataSource("~/DataSource/B7_CAPA_ONLINEDataSetKajian.Designer.cs", DT7);
                 this.ReportViewer1.LocalReport.DataSources.Add(DataSource7);
                 ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSetKajian", DT7));
 
-                ReportDataSource DataSource8 = new ReportDataSource("B7_CAPA_ONLINEDataSetEvaluator", DT8);
+                ReportDataSource DataSource8 = new ReportDataSource("~/DataSource/B7_CAPA_ONLINEDataSetEvaluator.Designer.cs", DT8);
                 this.ReportViewer1.LocalReport.DataSources.Add(DataSource8);
                 ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSetEvaluator", DT8));
             }
