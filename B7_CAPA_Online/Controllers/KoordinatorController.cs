5cb836a863f107e5d03f151f593b79362ea10c5b
TANAEL BUDIMAN <tanael.budiman@binus.ac.id>
2022-02-04 16:46:46 +0700

    mengubah table hris ke table user all apps, dan menambahkan trigger CAPA Nomor CAPA

--- B7_CAPA_Online/Controllers/KoordinatorController.cs
+++ B7_CAPA_Online/Controllers/KoordinatorController.cs
@@ -66,7 +66,7 @@ namespace B7_CAPA_Online.Controllers
         {
             return View();
         }
-        public ActionResult PenilaianEfektivitas(string NoCAPA, string status)
+        public ActionResult MonitoringEfektivitas(string NoCAPA, string status)
         {
             ViewBag.NoCAPA = NoCAPA;
             ViewBag.status = status;
