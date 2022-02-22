f033aa96616a7cff6d38635b1a2cbbbc59a1671f
nocturnal <classieroof@gmail.com>
2022-02-22 17:26:26 +0700

    22/02/2022 Perubahan tampilan FormCAPA

--- B7_CAPA_Online/Controllers/PICController.cs
+++ B7_CAPA_Online/Controllers/PICController.cs
@@ -94,6 +94,12 @@ namespace B7_CAPA_Online.Controllers
             string Return = DAL.StoredProcedure(parameters, "[dbo].[SP_FORM_CAPA]");
             return Json(Return);
         }
+        public ActionResult GetWhyList(DynamicModel Param)
+        {
+            DynamicParameters parameters = new DynamicParameters(Param.Model);
+            string Return = DAL.StoredProcedure(parameters, "[dbo].[SP_FORM_CAPA]");
+            return Json(Return);
+        }
         #endregion
 
         #region Execute
