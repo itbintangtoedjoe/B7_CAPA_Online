b9a948e1207929c864bca18c9af1a9c8541319a0
tanael budiman <tanaelbudiman@gmail.com>
2022-05-30 10:16:07 +0700

    menambahkan blockui untuk di pending task.

--- B7_CAPA_Online/Scripts/CheckSession.cs
+++ B7_CAPA_Online/Scripts/CheckSession.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Http.Filters;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -18,13 +19,30 @@ namespace B7_CAPA_Online.Scripts
             {
                 //filterContext.HttpContext.Response.StatusCode = 403;
                 //filterContext.Result = new JsonResult { Data = "LogOut" };
-                filterContext.Result = new RedirectToRouteResult(
-                   new RouteValueDictionary {
-                                { "Controller", "Login" },
-                                { "Action", "Session_Error" }
-                               });
+                if (filterContext.HttpContext.Request.IsAjaxRequest())
+                {
+                    filterContext.HttpContext.Response.StatusCode = 401;
+                }
+                //if (filterContext.HttpContext.Request.IsAjaxRequest())
+                //{
+                //    var model = new
+                //    {
+                //        AjaxFailed = "true",
+                //        RedirectURL = "localhost/home/index"
+                //    };
+                //    //filterContext.Result = new JsonResult { Data = "LogOut" };
+                //}
+                else
+                {
+                    filterContext.Result = new RedirectResult("~/Login/Session_Error");
+                }
+                //filterContext.Result = new RedirectToRouteResult(
+                //   new RouteValueDictionary {
+                //                { "Controller", "Login" },
+                //                { "Action", "Session_Error" }
+                //               });
+                base.OnActionExecuting(filterContext);
             }
-            base.OnActionExecuting(filterContext);
         }
     }
 }
